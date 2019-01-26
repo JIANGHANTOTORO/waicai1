95ae419d826a58c3f5561b310f874533ef883098
JIANGHANTOTORO <m18896650173@163.com>
2019-01-26 23:09:52 +0800

    Initial commit

---
+++ src/main/java/com/youkeda/waicai/WaicaiApplication.java
@@ -0,0 +1,14 @@
+package com.youkeda.waicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaicaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaicaiApplication.class, args);
+	}
+
+}
+
